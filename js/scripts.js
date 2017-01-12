8a9d49c6986e7667f3eae1f3f81dfd9d428046cc
Benjamin T. Seaver <BenjaminTSeaver@gmail.com>
2017-01-12 13:32:07 -0800

    correct content, add JavaScript and CSS to initially hide then show content

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 $(function() {
+  // Note \n signals a new line.
+  var message =
+    "In Oregon one may vote at 18 years old and register at 17.\n" +
+    "Click OK if you are old enough to vote or register.";
+
+  if (confirm(message)) {
+    $("#oldEnoughToVote").show();
+  } else {
+    $("#notOldEnoughToVote").show();
+  };
 }); // End JavaScript
